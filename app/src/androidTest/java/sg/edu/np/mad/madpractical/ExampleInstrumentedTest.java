46d0fbb6d75ae2c2b9b203d05e6837645ab98371
benwoo1110 <30431861+benwoo1110@users.noreply.github.com>
2021-05-03 14:57:45 +0800

    Try the integrated testing.

--- app/src/androidTest/java/sg/edu/np/mad/madpractical/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/sg/edu/np/mad/madpractical/ExampleInstrumentedTest.java
@@ -1,13 +1,20 @@
 package sg.edu.np.mad.madpractical;
 
 import android.content.Context;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+import androidx.test.espresso.NoMatchingViewException;
+import androidx.test.espresso.ViewAssertion;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.*;
 
 /**
@@ -22,5 +29,15 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("sg.edu.np.mad.madpractical", appContext.getPackageName());
+    }
+
+    @Test
+    public void failLol() {
+        // Context of the app under test.
+        onView(withId(R.id.buttonFollow)).check((view, noViewFoundException) -> {
+            String followText = ((Button) view).getText().toString();
+            assertEquals("follow", followText);
+            assertEquals("unfollow", followText);
+        })
     }
 }
